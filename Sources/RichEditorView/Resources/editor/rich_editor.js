c3dc4a8baf6b4211158c4d104b1bc2211abf7688
rushangfynd <rushangprajapati@fynd.com>
2023-08-31 13:10:40 +0530

    Update rich_editor.js

--- Sources/RichEditorView/Resources/editor/rich_editor.js
+++ Sources/RichEditorView/Resources/editor/rich_editor.js
@@ -40,7 +40,8 @@ document.addEventListener('click', function(event) {
 // Get the HTML tag of the clicked element
     const tagName = event.target.tagName;
 // You can replace this code with any action you want to perform when a click happens.
-    console.log(`Clicked at (${clickX}, ${clickY}). Element tag: ${tagName}`);
+
+     RE.callback("Clicked at (${clickX}, ${clickY}). Element tag: ${tagName}");
 });
 
 
